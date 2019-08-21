5632f7f26728666aa4d8116ea81b0d5ba1ef2c1b
Pascal Jungblut <pascal.jungblut@ifi.lmu.de>
2019-08-21 17:33:13 +0200

    Add reduce kernel

--- mephisto/include/mephisto/internal/kernels.hpp
+++ mephisto/include/mephisto/internal/kernels.hpp
@@ -3,6 +3,9 @@
 #include <alpaka/alpaka.hpp>
 #include <mephisto/internal/iterator.hpp>
 
+namespace mephisto {
+namespace execution {
+
 template <typename T, uint64_t size>
 struct cheapArray
 {
@@ -37,10 +40,19 @@ struct cheapArray
 //! \tparam TBlockSize The block size.
 //! \tparam T The data type.
 //! \tparam TFunc The Functor type for the reduction function.
-template <size_t TBlockSize, typename T, typename TFunc>
+template <typename TFunc>
 struct ReduceKernel
 {
     ALPAKA_NO_HOST_ACC_WARNING
+
+    TFunc func;
+
+    ReduceKernel(TFunc func)
+      : func(func)
+    {
+    }
+
+    static constexpr std::size_t TBlockSize = 256;
 
     //-----------------------------------------------------------------------------
     //! The kernel entry point.
@@ -54,94 +66,120 @@ struct ReduceKernel
     //! \param destination The destination memory.
     //! \param n The problem size.
     //! \param func The reduction function.
-    template <typename TAcc, typename TElem, typename TIdx>
-    ALPAKA_FN_ACC auto operator()(TAcc const &acc,
-                                  TElem const *const source,
-                                  TElem *destination,
-                                  TIdx const &n,
-                                  TFunc func) const -> void
+
+    template <typename TAcc, typename TElem, typename TRes, typename TIdx>
+    ALPAKA_FN_ACC void operator()(
+        TAcc const &      acc,
+        const std::size_t block_index,
+        TElem *const      source,
+        TRes *            destination,
+        TIdx const &      n) const
     {
-        auto &sdata(
-            alpaka::block::shared::st::allocVar<cheapArray<T, TBlockSize>,
-                                                __COUNTER__>(acc));
+      /* std::cout << "reduce kernel" << std::endl; */
+      auto &sdata(
+          alpaka::block::shared::st::
+              allocVar<cheapArray<TRes, TBlockSize>, __COUNTER__>(acc));
 
-        const uint32_t blockIndex(static_cast<uint32_t>(
-            alpaka::idx::getIdx<alpaka::Grid, alpaka::Blocks>(acc)[0]));
-        const uint32_t threadIndex(static_cast<uint32_t>(
-            alpaka::idx::getIdx<alpaka::Block, alpaka::Threads>(acc)[0]));
-        const uint32_t gridDimension(static_cast<uint32_t>(
-            alpaka::workdiv::getWorkDiv<alpaka::Grid, alpaka::Blocks>(acc)[0]));
+      const uint32_t blockIndex(static_cast<uint32_t>(
+          alpaka::idx::getIdx<alpaka::Grid, alpaka::Blocks>(acc)[0]));
+      const uint32_t threadIndex(static_cast<uint32_t>(
+          alpaka::idx::getIdx<alpaka::Block, alpaka::Threads>(acc)[0]));
+      const uint32_t gridDimension(static_cast<uint32_t>(
+          alpaka::workdiv::getWorkDiv<alpaka::Grid, alpaka::Blocks>(acc)[0]));
 
-        // equivalent to blockIndex * TBlockSize + threadIndex
-        const uint32_t linearizedIndex(static_cast<uint32_t>(
-            alpaka::idx::getIdx<alpaka::Grid, alpaka::Threads>(acc)[0]));
+      // equivalent to blockIndex * TBlockSize + threadIndex
+      const uint32_t linearizedIndex(static_cast<uint32_t>(
+          alpaka::idx::getIdx<alpaka::Grid, alpaka::Threads>(acc)[0]));
 
-        IteratorCpu<TAcc, TElem> it(
-            acc, source, linearizedIndex, gridDimension * TBlockSize, n);
+      IteratorCpu<TAcc, TElem> begin(
+          acc, source, linearizedIndex, gridDimension * TBlockSize, n);
+      auto it = begin;
 
-        T result = 0; // suppresses compiler warnings
+      // doing the remaining blocks
+      while (it < it.end()) func(blockIndex, 0, sdata.data, &(*it++));
 
-        if (threadIndex < n)
-            result = *(it++); // avoids using the
-                              // neutral element of specific
+      alpaka::block::sync::syncBlockThreads(acc);
 
-        // --------
-        // Level 1: grid reduce, reading from global memory
-        // --------
+      // --------
+      // Level 2: block + warp reduce, reading from shared memory
+      // --------
 
-        // reduce per thread with increased ILP by 4x unrolling sum.
-        // the thread of our block reduces its 4 grid-neighbored threads and
-        // advances by grid-striding loop (maybe 128bit load improve perf)
+      printf("sdata: ");
+      for(size_t i = 0; i < TBlockSize; i++) printf("%d ", sdata[i]);
+      ALPAKA_UNROLL()
+      for (uint32_t currentBlockSize = TBlockSize,
+                    currentBlockSizeUp =
+                        (TBlockSize + 1) / 2;  // ceil(TBlockSize/2.0)
+           currentBlockSize > 1;
+           currentBlockSize            = currentBlockSize / 2,
+                    currentBlockSizeUp = (currentBlockSize + 1) /
+                                         2)  // ceil(currentBlockSize/2.0)
+      {
+        bool cond =
+            threadIndex < currentBlockSizeUp  // only first half of block
+                                              // is working
+            && (threadIndex + currentBlockSizeUp) <
+                   TBlockSize  // index for second half must be in bounds
+            && (blockIndex * TBlockSize + threadIndex + currentBlockSizeUp) <
+                   n &&
+            threadIndex <
+                n;  // if elem in second half has been initialized before
 
-        while (it + 3 < it.end())
-        {
-            result = func(
-                func(func(result, func(*it, *(it + 1))), *(it + 2)), *(it + 3));
-            it += 4;
+        if (cond) {
+          sdata[threadIndex] = sdata[threadIndex] + sdata[threadIndex + currentBlockSizeUp];
         }
-
-        // doing the remaining blocks
-        while (it < it.end())
-            result = func(result, *(it++));
-
-        if (threadIndex < n)
-            sdata[threadIndex] = result;
 
         alpaka::block::sync::syncBlockThreads(acc);
+      }
 
-        // --------
-        // Level 2: block + warp reduce, reading from shared memory
-        // --------
-
-        ALPAKA_UNROLL()
-        for (uint32_t currentBlockSize = TBlockSize,
-                      currentBlockSizeUp =
-                          (TBlockSize + 1) / 2; // ceil(TBlockSize/2.0)
-             currentBlockSize > 1;
-             currentBlockSize = currentBlockSize / 2,
-                      currentBlockSizeUp = (currentBlockSize + 1) /
-                                           2) // ceil(currentBlockSize/2.0)
-        {
-            bool cond =
-                threadIndex < currentBlockSizeUp // only first half of block
-                                                 // is working
-                && (threadIndex + currentBlockSizeUp) <
-                       TBlockSize // index for second half must be in bounds
-                && (blockIndex * TBlockSize + threadIndex +
-                    currentBlockSizeUp) < n &&
-                threadIndex <
-                    n; // if elem in second half has been initialized before
-
-            if (cond)
-                sdata[threadIndex] =
-                    func(sdata[threadIndex],
-                         sdata[threadIndex + currentBlockSizeUp]);
-
-            alpaka::block::sync::syncBlockThreads(acc);
-        }
-
-        // store block result to gmem
-        if (threadIndex == 0 && threadIndex < n)
-            destination[blockIndex] = sdata[0];
+      // store block result to gmem
+      if (threadIndex == 0 && threadIndex < n) {
+        printf("\ndestination[%zu]: %d\n", block_index, sdata[0]);
+        destination[block_index] = sdata[0];
+      }
     }
 };
+
+template <typename F>
+class UniversalKernel {
+  F _func;
+public:
+
+  UniversalKernel(F func) : _func(func) {}
+
+  ALPAKA_NO_HOST_ACC_WARNING
+  template <typename TAcc, typename TElem, typename TRes, typename TIdx>
+  ALPAKA_FN_ACC auto operator()(
+      TAcc const &      acc,
+      const std::size_t block_index,
+      TElem *const      A,
+      TRes *            res,
+      TIdx const &      numElements) const -> void
+  {
+    const uint32_t blockIndex(static_cast<uint32_t>(
+        alpaka::idx::getIdx<alpaka::Grid, alpaka::Blocks>(acc)[0]));
+    const uint32_t threadIndex(static_cast<uint32_t>(
+        alpaka::idx::getIdx<alpaka::Block, alpaka::Threads>(acc)[0]));
+    const uint32_t gridDimension(static_cast<uint32_t>(
+        alpaka::workdiv::getWorkDiv<alpaka::Grid, alpaka::Blocks>(acc)[0]));
+    const uint32_t threadElementExtent(static_cast<uint32_t>(
+        alpaka::workdiv::getWorkDiv<alpaka::Thread, alpaka::Elems>(acc).prod()));
+
+    const uint32_t i(static_cast<uint32_t>(
+        alpaka::idx::getIdx<alpaka::Grid, alpaka::Threads>(acc)[0]));
+
+    TIdx const threadFirstElemIdx(i * threadElementExtent);
+
+    if (threadFirstElemIdx < numElements) {
+      TIdx const threadLastElemIdx(threadFirstElemIdx + threadElementExtent);
+      TIdx const threadLastElemIdxClipped(
+          (numElements > threadLastElemIdx) ? threadLastElemIdx
+                                            : numElements);
+      for (TIdx a(threadFirstElemIdx); a < threadLastElemIdxClipped; ++a) {
+        _func(i, a, res, A);
+      }
+    }
+  }
+};
+}  // namespace execution
+}  // namespace mephisto
